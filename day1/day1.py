089290dab118deb3e68e83cc157b5ac44365d2d3
bjolley74 <bobby@bjolley.com>
2020-12-02 09:32:03 -0600

    day 1 complete

--- day1/day1.py
+++ day1/day1.py
@@ -18,14 +18,33 @@ def make_points(list_in: list):
             if item1 != item2:
                 yield item1, item2
 
+def make_3_points(list_in: list):
+    list_b = copy.deepcopy(list_in)
+    list_c = copy.deepcopy(list_in)
+    for item1 in list_in:
+        for item2 in list_b:
+            for item3 in list_c:
+                if item1 != item2 and item2 != item3 and item1 != item3:
+                    yield item1, item2, item3
 
-def calc_ans(puzzle_list: list) -> int:
+def calc_ans1(puzzle_list: list) -> int:
     for one, two in make_points(puzzle_list):
         if one + two == 2020:
             return one * two
 
+def calc_ans2(puzzle_list: list) -> int:
+    for one, two, three in make_3_points(puzzle_list):
+        if one + two + three == 2020:
+            return one * two * three
+
 if __name__ == "__main__":
-    if calc_ans(test_input) == 514579:
+    if calc_ans1(test_input) == 514579:
         print("Please wait while I calculate answer...")
         puzzle_input = get_input()
-        print(f'answer = {calc_ans(puzzle_input)}')
+        print(f'answer one is {calc_ans1(puzzle_input)}')
+    else:
+        print(f"answer not correct, {calc_ans1(test_input)}")
+    if calc_ans2(test_input) == 241861950:
+        print(f"answer two is {calc_ans2(puzzle_input)}")
+    else:
+        print(f"answer not correct, {calc_ans2(test_input)}")
