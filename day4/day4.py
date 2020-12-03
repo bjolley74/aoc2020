cf9c14907d6aaf003d175fc9f54f6dd4f15f15be
bjolley74 <bobby@bjolley.com>
2020-12-03 10:39:39 -0600

    updated new_day.py to add more structure

--- day4/day4.py
+++ day4/day4.py
@@ -1,9 +1,26 @@
 from aoc_input import AOCInput
 
+def ans1():
+    return 0
 
+def ans2():
+    return 0
+
+def test1():
+    return False
+
+def test2():
+    return False
 
 def main():
-	pass
+    if test1():
+        answer1 = ans1()
+        print(f'The answer for puzzle one is {answer1}')
+    else:        print('Test1 failed')
+    if test2():
+        answer2 = ans2()
+        print(f'The answer for puzzle two is {answer2}')
+    else:        print('Test2 failed')
 
 if __name__ == "__main__":
-	main()
+    main()
