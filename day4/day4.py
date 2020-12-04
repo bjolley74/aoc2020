c9d0d3b4874d7e1c554b6ad1e9bbe2d416136f69
bjolley74 <bobby@bjolley.com>
2020-12-04 09:53:47 -0600

    puzzle 1 done

--- day4/day4.py
+++ day4/day4.py
@@ -1,24 +1,55 @@
-from aoc_input import AOCInput
+def get_input(filename):
+    with open(filename, 'r') as file:
+        lines = file.read().replace(' ',', ')
+    passports = lines.split('\n\n')
+    passports_out = []
+    for p in passports:
+        a = p.split(', ')
+        my_dict = {}
+        for b in a:
+            if '\n' in b:
+                c = b.split('\n')
+                for d in c:
+                    e, f = d.split(':')
+                    my_dict[e] = f
+            else:
+                c, d = b.split(':')
+                my_dict[c] = d
+        passports_out.append(my_dict)
+    return passports_out
 
-def ans1():
-    return 0
+def det_valid(passport:dict) -> bool:
+    validity = True
+    required_fields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
+    for field in required_fields:
+        if field not in passport:
+            validity = False 
+    return validity
 
-def ans2():
-    return 0
+def ans(passports: list)-> int:
+    count_valid = 0
+    for passport in passports:
+        if det_valid(passport):
+            count_valid += 1
+    return count_valid
+
 
 def test1():
-    return False
+    passports = get_input('test.input')
+    answer = ans1(passports)
+    return answer == 2
 
 def test2():
     return False
 
 def main():
     if test1():
-        answer1 = ans1()
+        answer1 = ans(get_input('puzzle_input.txt'))
         print(f'The answer for puzzle one is {answer1}')
-    else:        print('Test1 failed')
+    else:
+        print('Test1 failed')
     if test2():
-        answer2 = ans2()
+        answer2 = ans(get_input('puzzle_input.txt'))
         print(f'The answer for puzzle two is {answer2}')
     else:        print('Test2 failed')
 
