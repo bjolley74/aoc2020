da1c95abc0a22189c2174b945345636ed7fe2225
bjolley74 <bobby@bjolley.com>
2020-12-09 11:12:14 -0600

    updated new_day.py

--- new_day.py
+++ new_day.py
@@ -8,24 +8,6 @@ current_directory = Path(os.getcwd())
 if current_directory != target_dir:
     os.chdir(target_dir)
 
-def write_file(file_obj):
-    file_obj.write('from aoc_input import AOCInput\n\n')
-    file_obj.write('test_input = AOCInput(filename="test_input.py")\n')
-    file_obj.write('puzzle_input = AOCInput()\n\n')
-    file_obj.write('# ans() finds answer to the puzzle\n')
-    file_obj.write('def ans(data_in: list):\n    return 0\n\n')
-    file_obj.write('#tests ans on test input\n')
-    file_obj.write('def test_ans(data_in: list) -> bool:\n    return False\n\n')
-    file_obj.write('def main():\n')
-    file_obj.write('    if test_ans(test_input):\n')
-    file_obj.write('        answer = ans(puzzle_input)\n')
-    file_obj.write("        print(f'The answer for puzzle is {answer}')\n")
-    file_obj.write("    else:\n")
-    file_obj.write("        print('Test failed')\n\n")
-    file_obj.write('if __name__ == "__main__":\n')
-    file_obj.write('    main()\n')
-    return "file written"
-
 def main():
     max_num = 0
     directories = glob('day*')
@@ -47,15 +29,13 @@ def main():
         print(f'\nError occured changing directory to {new_folder_name}\n')
     #write new .py file for part 1
     try:
-        with open('part1.py','w') as python_file:
-            write_file(python_file)
+        copyfile('../template.py', 'part1.py')
     except (IOError, OSError) as err:
         print(f'\nError occured while writing part1.py\n')
         raise err
     #write new .py file for part 2
     try:
-        with open('part2.py','w') as python_file:
-            write_file(python_file)
+        copyfile('../template.py', 'part2.py')
     except (IOError, OSError) as err:
         print(f'\nError occured while writing part2.py\n')
         raise err
