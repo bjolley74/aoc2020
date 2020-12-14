7e99153a8b48caeabdaa2ce2a0b05be128562bdf
bjolley74 <bobby@bjolley.com>
2020-12-14 09:39:38 -0600

    updated .gitignore to ignore .idea folder from pycharm

--- day10/part1.py
+++ day10/part1.py
@@ -9,12 +9,11 @@ logging.basicConfig(level=log_level, filename=log_file, filemode='w+', format=f)
 logger = logging.getLogger(__name__)
 
 
-
 def log_wrap(pre, post):
     """Wrapper"""
     def decorate(func):
         """Decorater"""
-        def call(*args,**kwargs):
+        def call(*args, **kwargs):
             """Actual Wrapper"""
             pre(func)
             result = func(*args, **kwargs)
@@ -28,14 +27,16 @@ def entering(func):
     """Pre function logging"""
     logger.debug(f"entered {func.__name__}")
 
+
 def exiting(func):
     """Post function logging"""
     logger.debug(f"exiting {func.__name__}")
 
+
 # get_data section
 @log_wrap(entering, exiting)
 def get_data(fn: str) -> list:
-    data_in  = AOCInput(filename=fn)
+    data_in = AOCInput(filename=fn)
     data = data_in.get_input
     #insert logic to change data as necessary
     return [int(x) for x in data]
@@ -46,12 +47,14 @@ def ans(data_in: list):
     """ans() finds answer to the puzzle"""
     return 0
 
+
 @log_wrap(entering, exiting)
 def test_ans(data_in: list) -> bool:
     """test ans() on test_input.txt"""
     result = ans(data_in)
     # set return to check that ans == expected result
     return result == 1000000
+
 
 @log_wrap(entering, exiting)
 def main():
@@ -69,6 +72,7 @@ def main():
         else:
             #prints if test failed
             print('Test failed')
+
 
 if __name__ == "__main__":
     main()
