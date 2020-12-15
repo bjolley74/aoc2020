97772ea9c403f30a4f8dc752b9f87f2589116678
bjolley74 <bobby@bjolley.com>
2020-12-15 15:03:11 -0600

    finished part1

--- day15/part1.py
+++ day15/part1.py
@@ -32,50 +32,47 @@ def exiting(func):
     logger.debug(f"exiting {func.__name__}")
 
 
-# get_data section
-@log_wrap(entering, exiting)
-def get_data(fn: str) -> list:
-    data_in  = AOCInput(filename=fn)
-    data = data_in.get_input
-    # insert logic to change data as necessary
-    output = data
-    return output
-
-
 @log_wrap(entering, exiting)
 def ans(starting_numbers: list):
     """ans() finds answer to the puzzle"""
     numbers_spoken = dict()
-    for x  in range(2020):
+    for x  in range(1,2021):
         logger.debug(f'x = {x}')
-        if x < len(starting_numbers):
-            numbers_spoken[x] = starting_numbers[x]
-            logger.debug(f'less than len starting numbers, added {starting_numbers[x]}')
+        if x < len(starting_numbers) + 1:
+            numbers_spoken[x] = starting_numbers[x-1]
+            logger.debug(f'less than len starting numbers, added {starting_numbers[x-1]}')
         else:
             logger.debug('x greater than len start nums')
             ns_values = list(numbers_spoken.values())
-            if numbers_spoken[x-1] in ns_values[:-2]:
-                logger.debug(f'number exists: {numbers_spoken[x-1]}')
+            number = numbers_spoken[x-1]
+            logger.debug(f'number = {number}')
+            if number in ns_values[:x-2]:
+                logger.debug(f'number exists: {number}')
                 keys_of_matches = []
                 for k, v in numbers_spoken.items():
                     if v == numbers_spoken[x-1]:
                         keys_of_matches.append(k)
                 logger.debug(f'keys_of_matches = {keys_of_matches}')
-                logger.debug(f'popped {keys_of_matches.pop()} from list')
+                pop_key = keys_of_matches.pop()
+                logger.debug(f'popped {pop_key} from list')
                 max_key = max(keys_of_matches)
-                age = x-max_key
+                logger.debug(f'max_key = {max_key}')
+                age = (x-1)-max_key
+                logger.debug(f'age = {age}')
                 numbers_spoken[x] = age
+                logger.debug(f'added {numbers_spoken[x]}')
             else:
-                logger.debug(f'first_time: {numbers_spoken[x-1]}')
+                logger.debug(f'first_time: {number}')
                 numbers_spoken[x] = 0
-    return numbers_spoken[2019]
+                logger.debug(f'added {numbers_spoken[x]}')
+    return numbers_spoken[2020]
 
 
 @log_wrap(entering, exiting)
 def main():
-    test = [0,3,6]
+    # puzzle = [0,3,6]
     puzzle = [0,14,1,3,7,9]
-    print(f"The answer for puzzle is {ans(test)}")
+    print(f"The answer for puzzle is {ans(puzzle)}")
 
 
 if __name__ == "__main__":
